bad519207236995c904c95328d92efe462cf370f
GitUserSada <sada_mule@yahoo.com>
2018-07-26 23:32:49 +0530

    added sonar

--- src/main/java/com/sada/demos/cassandra/DemoApplication.java
+++ src/main/java/com/sada/demos/cassandra/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.sada.demos.cassandra;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
@@ -14,6 +15,10 @@ public class DemoApplication {
 
 	@Autowired
 	Cluster cluster;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 	
 	@Bean
 	public QueryLogger queryLogger(Cluster cluster) {
